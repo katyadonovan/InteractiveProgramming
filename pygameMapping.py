b46faa330555869ae0859774d66b89d014e8b7db
Katya <katya@computer.com>
2017-03-06 23:11:35 -0500

    Pygame again!

--- pygameMapping.py
+++ pygameMapping.py
@@ -7,6 +7,72 @@ License: MIT LICENSE
 """
 import csv
 import pygame
+import pandas as pd
+import numpy as np
+import xlrd
+import math
+
+def processing_data():
+    my_dic_first_language = pd.read_excel('data2.xlsx', index_col=0, parse_cols = "A:B").to_dict()
+    my_dic_second_language = pd.read_excel('data2.xlsx', index_col=0, parse_cols = "A,C").to_dict()
+    my_dic_third_language = pd.read_excel('data2.xlsx', index_col=0, parse_cols = "A,D").to_dict()
+
+    a= my_dic_first_language['Dari Persian']
+    b = my_dic_second_language['Pashtu']
+    c = my_dic_third_language['Turkic']
+
+    country = pd.read_excel(open('country_centroids_all2.xlsx', 'rb'), parse_cols = "A")
+    position =  pd.read_excel('country_centroids_all2.xlsx', index_col=0, parse_cols = "A,D").to_dict()
+    position2 = pd.read_excel('country_centroids_all2.xlsx', index_col=0, parse_cols = "A,E").to_dict()
+
+    d =  position['LAT']
+    h = position2['LONG']
+
+    ps = pd.read_excel('data2.xlsx')
+    f=ps.iloc[:,0].tolist()
+    cd = pd.read_excel('country_centroids_all2.xlsx')
+    e=cd.iloc[:,0].tolist()
+
+    real_countries = []
+    for i in cd.iloc[:,0].tolist():
+        if i in ps.iloc[:,0].tolist():
+            real_countries.append(i)
+    for i in ps.iloc[:,0].tolist():
+        if i in cd.iloc[:,0].tolist():
+            real_countries.append(i)
+
+    new_first = {}
+    new_second = {}
+    new_third = {}
+    new_pos = {}
+    new_pos2 = {}
+
+    def common_dicts(dict1,dict2,lst):
+        for k,v in dict1.items():
+            for i in lst:
+                if i == k:
+                    dict2[k]=v
+        return dict2
+
+    new1= common_dicts(a,new_first,real_countries)
+    new2 = common_dicts(b,new_second,real_countries)
+    new3 = common_dicts(c,new_third,real_countries)
+    new4 = common_dicts(d,new_pos,real_countries)
+    new5 = common_dicts(h,new_pos2,real_countries)
+    dicts = [new1,new2,new3,new4,new5]
+
+    final_df =pd.DataFrame(dicts)
+    print(final_df.head())
+    #print(list(final_df.rows.values))
+    lang1 = list(final_df.iloc[0])
+    lang2 = list(final_df.iloc[1])
+    lang2 = list(final_df.iloc[2])
+    lat = list(final_df.iloc[3])
+    lon = list(final_df.iloc[4])
+    return final_df
+
+processing_data()
+
 
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
@@ -22,7 +88,7 @@ with open('country_centroids_primary.csv', 'rb') as csvfile:
 
 class Point(object):
         def __init__(self):
-            self.radius = 10
+            self.radius = 5
             self.language = 1
             self.name = 1
             self.x = 320
@@ -77,7 +143,7 @@ def main():
     # views.append(BallView(ball))
     views.append(PointView(point))
     # views.append(BallEnergyView(ball))
-    BackGround = Background('/home/sam/Documents/softDes/InteractiveProgramming/InteractiveProgramming/map.png', [0,0])
+    BackGround = Background('map.png', [0,0])
     # controller = BounceController([ball])
     controller = PointController([point])
 
